7ad66f9ebdcc984c9999d9bea0539b92e3f4c308
alexey.stratulat <alexey.stratulat@demlabs.net>
2021-06-16 13:14:16 +0700

    [+] Added function for getting a date in a specified format.

--- Cellframe-explorer_common.php
+++ Cellframe-explorer_common.php
@@ -55,3 +55,26 @@ function chains_html($chains_str, $default_chain){
     for ($i = 0; $i < count($chains); $i++){}
     return $chains;
 }
+
+function get_date_format($date){
+    $dt = explode(" ", $date);
+    $count_dt = count($dt);
+    if ($count_dt == 5) {
+        if ($dt[2] <= 9) {
+            $dt[2] = '0'.$dt[2];
+        }
+        return "<span>" . $dt[2] . " " . $dt[1] . " " . $dt[4] . "</span> <span>" . $dt[3] . "</span>";
+    } else if ($count_dt == 7) {
+        if ($dt[4] <= 9) {
+            $dt[4] = '0'.$dt[4];
+        }
+        return "<span>" . $dt[4] . " " . $dt[2] . " " . $dt[6] . "</span> <span>" . $dt[5] . "</span>";
+    } else if ($count_dt ==6){
+        if ($dt[3] <= 9){
+            $dt[3] = '0'.$dt[3];
+        }
+        return "<span>" . $dt[3] . " " . $dt[1] . " " . $dt[5] . "</span> <span>" . $dt[4] . "</span>";
+    } else {
+        return $date;
+    }
+}
